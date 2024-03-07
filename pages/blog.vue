45889182c5176506637256ec3cd858fc8ad2f498
aim <ahmadhakim404@gmail.com>
2024-03-07 20:55:01 +0700

    cara menggunakan parameter route berbasis file

--- pages/blog.vue
+++
@@ -1,4 +0,0 @@
-<template>
-	<h1>Blog</h1>
-	<p>This is an blog page</p>
-</template>
---
+++ pages/blog/[slug].vue
@@ -0,0 +1,9 @@
+<script setup>
+const route = useRoute();
+console.log(route);
+</script>
+
+<template>
+	<h1>Blog</h1>
+	<p>This is an {{ route.params.slug }} page</p>
+</template>

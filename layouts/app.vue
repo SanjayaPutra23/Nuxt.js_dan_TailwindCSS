305cdb70b746bb895e685e33db95b4f58c970570
aim <ahmadhakim404@gmail.com>
2024-03-09 01:37:58 +0700

    cara konfigurasi seo page di nuxt

--- layouts/app.vue
+++ layouts/app.vue
@@ -13,6 +13,17 @@ useHead({
 		},
 	],
 });
+
+useSeoMeta({
+	title: 'Porto - My Amazing Site',
+	description: 'This is my amazing site, let me tell you all about it.',
+	ogTitle: 'My Amazing Site',
+	ogDescription: 'This is my amazing site, let me tell you all about it.',
+	ogImage: 'https://example.com/image.png',
+	twitterCard: 'summary_large_image',
+	twitterTitle: 'Porto - My Amazing Site',
+	twitterDescription: 'This is my amazing site, let me tell you all about it.',
+});
 </script>
 
 <template>

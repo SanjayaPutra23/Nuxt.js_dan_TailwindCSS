b793d36aa837d801d6902cbd623c1c68934ddeee
aim <ahmadhakim404@gmail.com>
2024-03-13 22:47:47 +0700

    mendapatkan daftar content, filtering dan memuat asset gambar public

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,14 +1,24 @@
 <script setup>
 definePageMeta({
-	layout: 'blog',
+	layout: 'app',
 });
 
 useHead({
 	title: 'Blog',
 });
+
+const { data: posts } = await useAsyncData('blog-list', () => {
+	return queryContent('blog').only(['_path', 'title']).find();
+});
 </script>
 
 <template>
-	<h1>Blog</h1>
-	<p>This is an blog page with post list</p>
+	<section class="prose dark:prose-invert">
+		<h1 class="text-2xl font-bold mb-10">Blog</h1>
+		<ul>
+			<li v-for="(post, index) in posts" :key="index">
+				<NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+			</li>
+		</ul>
+	</section>
 </template>

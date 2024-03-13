f29a6e2f96b02c7be1a54c641e96080408afd158
aim <ahmadhakim404@gmail.com>
2024-03-13 17:44:09 +0700

    menampilkan highlight pada konten yang berisi kode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,16 @@ export default defineNuxtConfig({
 	colorMode: {
 		classSuffix: '',
 	},
+	content: {
+		highlight: {
+			theme: {
+				// Default theme (same as single string)
+				default: 'github-light',
+				// Theme used if `html.dark`
+				dark: 'github-dark',
+				// Theme used if `html.sepia`
+				sepia: 'monokai',
+			},
+		},
+	},
 });
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -9,7 +9,9 @@ useHead({
 </script>
 
 <template>
-	<article class="prose dark:prose-invert">
+	<article
+		class="prose dark:prose-invert prose-pre:bg-gray-200 dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300"
+	>
 		<ContentDoc />
 	</article>
 </template>

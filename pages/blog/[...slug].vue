7786e19e3653906978e2d7e10e67c051950d645f
aim <ahmadhakim404@gmail.com>
2024-03-14 21:04:04 +0700

    persiapan untuk menampilkan table of content dari markdown

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -12,6 +12,12 @@ useHead({
 	<article
 		class="prose dark:prose-invert prose-pre:bg-gray-200 dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300"
 	>
-		<ContentDoc />
+		<ContentDoc v-slot="{ doc }">
+			<pre>
+				{{ doc.body.toc.links }}
+			</pre
+			>
+			<ContentRenderer :value="doc" />
+		</ContentDoc>
 	</article>
 </template>

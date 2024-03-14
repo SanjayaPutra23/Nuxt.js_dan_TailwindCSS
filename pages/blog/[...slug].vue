590a79caceb1fab00a5cd401b657402b8a4d0d50
aim <ahmadhakim404@gmail.com>
2024-03-15 00:03:59 +0700

    menampilkan table of content

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -13,11 +13,19 @@ useHead({
 		class="prose dark:prose-invert prose-pre:bg-gray-200 dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300"
 	>
 		<ContentDoc v-slot="{ doc }">
-			<pre>
-				{{ doc.body.toc.links }}
-			</pre
-			>
-			<ContentRenderer :value="doc" />
+			<div class="grid grid-cols-6 gap-16">
+				<div class="col-span-4">
+					<ContentRenderer :value="doc" />
+				</div>
+				<div class="col-span-2 not-prose">
+					<aside class="sticky top-8">
+						<div class="font-semibold mb-2">Table of Contents</div>
+						<nav>
+							<TocLink :links="doc.body.toc.links" />
+						</nav>
+					</aside>
+				</div>
+			</div>
 		</ContentDoc>
 	</article>
 </template>

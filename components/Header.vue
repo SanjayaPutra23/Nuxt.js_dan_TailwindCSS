007dcb74e6a15349e54b96b55eb1c3225cbebe18
aim <ahmadhakim404@gmail.com>
2024-03-17 15:35:58 +0700

    mengubah desain project jadi responsive

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,11 @@
 <template>
-	<header class="flex justify-between items-center mt-5">
-		<div class="flex items-center space-x-12">
-			<div>
+	<header class="flex justify-between items-start md:items-center mt-5">
+		<div class="flex items-center md:space-x-12">
+			<div class="hidden md:block">
 				<NuxtLink to="/" class="text-xl font-semibold link">Porto</NuxtLink>
 			</div>
 			<nav class="font-mono">
-				<ul class="flex space-x-4">
+				<ul class="flex flex-col md:flex-row md:space-x-4">
 					<li><NuxtLink to="/" class="link">Home</NuxtLink></li>
 					<li><NuxtLink to="/about" class="link">About</NuxtLink></li>
 					<li><NuxtLink to="/project" class="link">Project</NuxtLink></li>
@@ -21,6 +21,6 @@
 
 <style scoped>
 .link {
-	@apply p-2 hover:bg-gray-200 dark:hover:bg-gray-700;
+	@apply p-2 hover:bg-gray-200 dark:hover:bg-gray-700 text-2xl  md:text-base;
 }
 </style>
